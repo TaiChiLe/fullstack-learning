f6372ae6b9c12d58dd8b9e699c8bb1571d4d17b3
Tyson Le <taichi.le3@gmail.com>
2024-04-21 08:32:32 +1000

    Ammendments to homework/week-12

--- week-12/homework/index.js
+++ week-12/homework/index.js
@@ -1,76 +1,30 @@
 let input = document.querySelector('.input');
 let addBtn = document.querySelector('.add-btn');
 let itemContainer = document.querySelector('.item-container');
-let deleteBtns = document.querySelectorAll('.delete-btn');
-let editBtns = document.querySelectorAll('.edit-btn');
 let currentTaskNum = 0;
-
 //Creates tasklist, takes taskNumber and input value
 let processTaskList = function (taskNum, inputValue) {
-    let newInput = document.createElement("input");
-    let deleteBtn = document.createElement("button");
-    let editBtn = document.createElement("button");
-    let saveBtn = document.createElement("button");
-    let newDiv = document.createElement("div");
 
-    deleteBtn.innerHTML = "X";
-    deleteBtn.setAttribute('delete-item-number', taskNum);
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.classList.add('btn');
-    deleteBtn.classList.add('btn-danger');
-    editBtn.innerHTML = "Edit";
-    editBtn.setAttribute('edit-item-number', taskNum);
-    editBtn.classList.add('edit-btn');
-    editBtn.classList.add('btn');
-    editBtn.classList.add('btn-primary');
-    saveBtn.innerHTML = "Save";
-    saveBtn.setAttribute('save-item-number', taskNum);
-    saveBtn.classList.add('save-btn');
-    saveBtn.classList.add('d-none');
-    saveBtn.classList.add('btn');
-    saveBtn.classList.add('btn-secondary');
-    newInput.type = "text";
-    newInput.setAttribute('disabled', 'true');
-    newInput.setAttribute('input-item-number', taskNum);
-    newInput.classList.add('input-item');
-    newInput.classList.add('form-control');
-    newInput.value = inputValue;
-    newDiv.classList.add("item");
-    newDiv.classList.add("d-flex");
-    newDiv.classList.add("pt-1")
-    newDiv.setAttribute('item-number', taskNum);
+    itemContainer.innerHTML = itemContainer.innerHTML + `
+    <div class="item d-flex pt-1" item-number="${taskNum}">
+    <input type="text" disabled="true" input-item-number="${taskNum}" class="input-item form-control" value="${inputValue}">
+    <button delete-item-number="${taskNum}" class="delete-btn btn btn-danger">X</button>
+    <button edit-item-number="${taskNum}" class="edit-btn btn btn-primary">Edit</button>
+    <button save-item-number="${taskNum}" class="save-btn d-none btn btn-secondary">Save</button></div>`;
 
-    newDiv.appendChild(newInput);
-    newDiv.appendChild(deleteBtn);
-    newDiv.appendChild(editBtn);
-    newDiv.appendChild(saveBtn);
-
-    itemContainer.appendChild(newDiv);
-
-
+    //clears input value from input section
     input.value = '';
-
-    deleteBtns = document.querySelectorAll('.delete-btn');
-    editBtns = document.querySelectorAll('.edit-btn');
-
-    for (let i = 0; i < deleteBtns.length; i++) {
-        deleteBtns[i].addEventListener('click', processDeleteBtn);
-    }
-
-    for (let i = 0; i < editBtns.length; i++) {
-        editBtns[i].addEventListener('click', processEditBtn);
-    }
+    //Add event listeners to the buttons
+    let currentDeleteBtn = document.querySelector(`[delete-item-number="${taskNum}"]`);
+    let currentEditBtn = document.querySelector(`[edit-item-number="${taskNum}"]`);
+    currentDeleteBtn.addEventListener('click', processDeleteBtn);
+    currentEditBtn.addEventListener('click', processEditBtn);
 }
-
 let processAddBtn = function (event) {
     processTaskList(currentTaskNum, input.value);
     currentTaskNum++;
 }
-
 addBtn.addEventListener('click', processAddBtn);
-
-
-
 //Process delete Btn, when clicked remove the html content and from the array:
 let processDeleteBtn = function (event) {
     let button = event.target;
@@ -78,7 +32,6 @@ let processDeleteBtn = function (event) {
     let targetItem = document.querySelector(`[item-number="${itemNum}"]`);
     targetItem.outerHTML = '';
 }
-
 //Process save Btn when clicked
 let processSaveBtn = function (event) {
     let button = event.target;
@@ -86,11 +39,10 @@ let processSaveBtn = function (event) {
     let textInput = document.querySelector(`[input-item-number="${itemNum}"]`);
     let saveBtn = document.querySelector(`[save-item-number="${itemNum}"]`);
     let editBtn = document.querySelector(`[edit-item-number="${itemNum}"]`);
-
+    textInput.setAttribute('value', textInput.value);
     textInput.disabled = true;
     saveBtn.classList.add('d-none');
     editBtn.classList.remove('d-none');
-
 }
 
 let processEditBtn = function (event) {
@@ -102,11 +54,8 @@ let processEditBtn = function (event) {
     targetItem.disabled = false;
     editBtn.classList.add('d-none');
     saveBtn.classList.remove('d-none');
-
     saveBtn.addEventListener('click', processSaveBtn);
-
 }
-
 
 let sortBtn = document.querySelector('.sort-btn');
 
@@ -125,7 +74,3 @@ let processSort = function () {
 }
 
 sortBtn.addEventListener('click', processSort);
-
-
-
-
