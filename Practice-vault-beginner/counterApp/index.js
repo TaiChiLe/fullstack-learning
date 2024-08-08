a176232bdcb01bc3e05d36f48c6ee5b54fa2a397
Tyson Le <taichi.le3@gmail.com>
2024-08-08 20:01:07 +1000

    CounterApp

--- Practice-vault-beginner/counterApp/index.js
+++ Practice-vault-beginner/counterApp/index.js
@@ -1,3 +1,25 @@
-const add = (a, b) => {
+const plusBtn = document.querySelector('.plus-btn');
+const minusBtn = document.querySelector('.minus-btn');
+const counter = document.querySelector('.counter');
 
-}
+const max = 10;
+const min = -10;
+let currentNum = Number(counter.innerHTML);
+
+plusBtn.addEventListener('click', () => {
+    currentNum++;
+    if (currentNum > max) {
+        currentNum--;
+    } else {
+        counter.innerHTML = currentNum;
+    }
+});
+
+minusBtn.addEventListener('click', () => {
+    currentNum--;
+    if (currentNum < min) {
+        currentNum++;
+    } else {
+        counter.innerHTML = currentNum;
+    }
+});
