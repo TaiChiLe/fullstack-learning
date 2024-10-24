f42f2ddb3369cc8acc9450b51ce27fc30fca7feb
Tyson Le <taichi.le3@gmail.com>
2024-10-24 19:31:06 +1100

    Commit

---
+++ week-30/practice-1/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+function App() {
+  return (
+    <div className="container">
+      <img
+        src="https://www.creativefabrica.com/wp-content/uploads/2022/12/11/Modern-Painting-Of-A-Nice-Ocean-Sunrise-51541457-1.png"
+        alt=""
+      />
+      <div className="title">The Misadventures of Saltybeard</div>
+      <div className="snippet">
+        Captain Saltybeard and his crew set sail on The Soggy Seagull in search
+        of the Lost Sea treasure. A playful pod of dolphins stole their compass,
+        leaving them lost. Relying on Saltybeard's dubious navigation, they
+        stumbled into a giant inflatable sea monster from a beach party. Though
+        they found no treasure, they returned with tales of being outwitted by
+        dolphins and a rubber Kraken.
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ week-30/practice-2/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+function App() {
+  return (
+    <div className="container">
+      <div className="image"></div>
+      <div className="article">
+        <div className="heading">
+          Seems like heaven<span className="decorator"></span>
+        </div>
+        <div className="quote">
+          "Just when you're in heaven, life hands you a pair of running shoes!"
+        </div>
+        <div className="text">
+          <div>
+            Max had always dreamed of the perfect Sunday: cozy bed, snacks
+            within reach, and every streaming service at his fingertips. Today,
+            that dream was finally coming true. He had stocked up on popcorn,
+            candy, and enough soda to float a small boat. Wrapped in his softest
+            blanket, he flicked on the TV and sank into the cushions. "This," he
+            thought, "is heaven."
+          </div>
+          <div>
+            Just as Max found the perfect movie, his phone buzzed. It was a
+            message from his friend Lisa: "Ready for our 5K run?" Max's eyes
+            widened in horror. He had completely forgotten! With a groan, he
+            looked from the TV to his running shoes by the door. "Well," he
+            sighed, "it seemed like heaven... while it lasted!"
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
